1b516aa5dc3a32ac210cf164b955c37a82824191
Mason Smith <masonium@gmail.com>
2023-11-22 19:25:01 -0800

    new binary for example spanner settings

---
+++ src/bin/example_spawner.rs
@@ -0,0 +1,12 @@
+use ron::ser::PrettyConfig;
+use rustyrocket::obstacle::spawner_settings::SpawnerSettings;
+use anyhow::Result;
+
+
+fn main() -> Result<()> {
+    let ss = SpawnerSettings::new();
+    let f = std::fs::File::options().create(true).write(true).open("assets/levels/base.spawner.ron")?;
+    ron::ser::to_writer_pretty(f, &ss, PrettyConfig::default().struct_names(true))?;
+
+    Ok(())
+}
